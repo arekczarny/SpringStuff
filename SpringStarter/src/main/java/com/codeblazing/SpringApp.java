298ed51fb253dea1809bd199850db6bf3987a674
Arek Czarny <a.czarnoglowski@onet.eu>
2018-03-12 01:11:07 +0100

    Spring Starter app initial commit

---
+++ SpringStarter/src/main/java/com/codeblazing/SpringApp.java
@@ -0,0 +1,13 @@
+package com.codeblazing;
+
+/**
+ * @author: Arek Czarnoglowski
+ * @created: 12.03.2018 00:54
+ * @version: 1.0
+ */
+public class SpringApp {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Spring App!!!");
+	}
+}
