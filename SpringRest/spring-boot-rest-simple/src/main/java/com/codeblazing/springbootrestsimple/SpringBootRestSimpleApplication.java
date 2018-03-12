4cc7c0a6cda6cd5935d777f22e4b3c5a799c6ca5
Arek Czarny <a.czarnoglowski@onet.eu>
2018-03-13 00:04:11 +0100

    Spring Boot REST service initial

---
+++ SpringRest/spring-boot-rest-simple/src/main/java/com/codeblazing/springbootrestsimple/SpringBootRestSimpleApplication.java
@@ -0,0 +1,12 @@
+package com.codeblazing.springbootrestsimple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestSimpleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestSimpleApplication.class, args);
+	}
+}
