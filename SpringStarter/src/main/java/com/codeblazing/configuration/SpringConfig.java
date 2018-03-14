d6b4ac13ece46c7101b9032dbedab99c72a10851
Arek Czarny <a.czarnoglowski@onet.eu>
2018-03-14 11:43:21 +0100

    Added SpringConfig class to SpringStarter

---
+++ SpringStarter/src/main/java/com/codeblazing/configuration/SpringConfig.java
@@ -0,0 +1,13 @@
+package com.codeblazing.configuration;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author: Arek Czarnoglowski
+ * @created: 14.03.2018 11:39
+ * @version: 1.0
+ */
+@Configuration
+public class SpringConfig {
+
+}
